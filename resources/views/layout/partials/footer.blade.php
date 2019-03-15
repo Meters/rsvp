e3329534c94d06c2e13b1161e9393c2028a65ac0
Meters <metersprower@yahoo.com>
2019-03-15 08:58:11 +0800

    added bootstrap + basic template

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-muted">
+     <div class="container">
+       <p class="float-right">
+         <a href="#">Back to top</a>
+       </p>
+       <p>&copy; Page 2019</p>
+       <p><a href="admin">admin</a></p>
+     </div>
+   </footer>
---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,7 @@
+<section class="jumbotron text-center">
+     <div class="container">
+       <h1 class="jumbotron-heading">You're Invited!</h1>
+       <p class="lead text-muted">Event - 20th April<br>19 Street Place, 2nd Floor</p>
+	   <p class="text-muted">Let us know if you can attend below!</p>
+     </div>
+</section>
